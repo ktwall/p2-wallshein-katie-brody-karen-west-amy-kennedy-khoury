0983f90414af1f5c096338107790529475c544a5
Khoury Kennedy <khoury.kennedy@ufl.edu>
2022-04-06 11:55:44 -0400

    Add click event/ alert to social

--- index.js
+++ index.js
@@ -2,8 +2,16 @@
 
 
 //footer//
-var footer = document.querySelector('footer');
-console.log(footer);
+document.getElementById('img1').onclick=function(){myFunction()};
+function myFunction(){
+    alert("You will be redirected to our Instagram"); 
+}
+document.getElementById('img2').onclick=function(){myFunction()};
+function myFunction(){
+    alert("You will be redirected to our Instagram"); 
+}
+
+
 
 
 
