b0f622d3843544784d2ab3f841357720d9ba470c
Katie Wallshein <kwallshein@ufl.edu>
2022-04-08 14:45:48 -0400

    additional pictures added

--- index.js
+++ index.js
@@ -1,3 +1,44 @@
+// burger menu //
+
+var menuButton = document.querySelector('.menu-btn')
+var closeButton = document.querySelector('.close-btn')
+var menu = document.querySelector('.menu')
+
+var menuOpen = false
+
+function toggleMenu() {
+  if (menuOpen) {
+    menu.style.transform = 'translateX(-250px)'
+    menuOpen = false
+  } else {
+    menu.style.transform = 'translateX(0)'
+    menuOpen = true
+  }
+}
+
+menuButton.onclick = toggleMenu
+closeButton.onclick = toggleMenu
+
+window.addEventListener("click", function(e) {
+  if (!menuOpen) return
+  if (e.target === menuButton || e.target === closeButton) return
+  if (!menu.contains(e.target)) toggleMenu()
+})
+
+window.addEventListener("keydown", function(e) {
+  if (!menuOpen) return
+  var linkList = document.querySelectorAll('.menu a')
+  var lastLink = linkList[linkList.length - 1]
+  if (e.key === 'Tab' && !e.shiftKey && document.activeElement === lastLink) {
+    closeButton.focus()
+    e.preventDefault()    
+  }
+  if (e.key === 'Tab' && e.shiftKey && document.activeElement === closeButton) {
+    lastLink.focus()
+    e.preventDefault()    
+  }
+})
+
 // about //
 
 function readmore() {
