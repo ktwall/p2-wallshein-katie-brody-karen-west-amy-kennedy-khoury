8879d0a9c9458d0054b6a7ca481618e2209e32fa
amywest1 <87080188+amywest1@users.noreply.github.com>
2022-04-09 21:40:10 -0400

    added modal with cat API and styled

--- index.js
+++ index.js
@@ -58,6 +58,51 @@ document.getElementById('img2').onclick = function(e) {
 }
 
 
+//modal button//
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn2");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+//Make it an API
+var catEl = document.getElementById('catFact')
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+  document.getElementById('catFact').innerHTML = ""
+  fetch('https://catfact.ninja/fact')
+  .then(function(response) {
+    return response.json()
+  })
+  .then(function(data) {
+    console.log(data.fact)
+
+    var div = document.createElement('div')
+    var para = document.createElement('p')
+    para.innerText = data.fact
+    div.appendChild(para)
+
+    catEl.appendChild(div)
+  })
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
 
 
 
